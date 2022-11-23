2a76c0579a4dba85910b150f88d39924b954cdaa
Elisee AMEGASSI <eliseeamegassi@gmail.com>
2022-11-23 06:39:32 +0000

    fix prettier errors

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,15 +1,15 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import {Navigate} from "react-router";
+import { Navigate } from 'react-router';
 
 const Users = lazy(() => import('containers/Users'));
 
 const App = () => {
   return (
-      <Routes>
-        <Route path="users/*" element={<Users />} />
-        <Route path="*" element={<Navigate to="/users" replace />} />
-      </Routes>
+    <Routes>
+      <Route path="users/*" element={<Users />} />
+      <Route path="*" element={<Navigate to="/users" replace />} />
+    </Routes>
   );
 };
 
--- Front/src/containers/Users/ViewUser.js
+++ Front/src/containers/Users/ViewUser.js
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
-import { getUser } from "../../api/users";
-import { useAsync } from "react-async";
-import {useParams} from "react-router";
+import { getUser } from '../../api/users';
+import { useAsync } from 'react-async';
+import { useParams } from 'react-router';
 import styled from 'styled-components';
 
 const FieldContainer = styled.div`
@@ -14,25 +14,25 @@ const ViewUser = () => {
   const { data } = useAsync({ promiseFn: getUser, userId });
   const user = data?.data;
   return (
-        <>
-          <FieldContainer>
-            <div>
-              {t('LOGIN')} : {user?.login}
-            </div>
-          </FieldContainer>
+    <>
+      <FieldContainer>
+        <div>
+          {t('LOGIN')} : {user?.login}
+        </div>
+      </FieldContainer>
 
-          <FieldContainer>
-            <div>
-              {t('EMAIL')} : {user?.email}
-            </div>
-          </FieldContainer>
+      <FieldContainer>
+        <div>
+          {t('EMAIL')} : {user?.email}
+        </div>
+      </FieldContainer>
 
-          <FieldContainer>
-            <div>
-              {t('PASSWORD')} : {user?.password}
-            </div>
-          </FieldContainer>
-        </>
+      <FieldContainer>
+        <div>
+          {t('PASSWORD')} : {user?.password}
+        </div>
+      </FieldContainer>
+    </>
   );
 };
 
